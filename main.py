d985da10d57eb2bba4993743f82bbf8c67d602ef
Abrorbek Patidinov <darkorb869@gmail.com>
2023-04-07 21:42:54 +0300

    Task 6: Do you use public transport? You probably paid for the fare and received a ticket with a number. A lucky ticket is a ticket with a six-digit number, where the sum of the first three digits is equal to the sum of the last three. Those. ticket number 385916 is lucky, because 3+8+5=9+1+6. You need to write a program that checks if a ticket is lucky.

--- main.py
+++ main.py
@@ -22,3 +22,22 @@ def petya_katya_seryozha_made_paper_cranes(s, x, k):
 
 # petya_katya_seryozha_made_paper_cranes(2, 2, 4)
 
+
+# Задача 6: Вы пользуетесь общественным транспортом? Вероятно, вы расплачивались за проезд и получали билет с номером.
+# Счастливым билетом называют такой билет с шестизначным номером, где сумма первых трех цифр равна сумме последних трех.
+# Т.е. билет с номером 385916 – счастливый, т.к. 3+8+5=9+1+6. Вам требуется написать программу, которая проверяет счастливость билета.
+def lucky_ticket():
+    print("Task №6")
+    a = input("Enter your numbers on your ticket: ")
+    sum_1 = int(a[0]) + int(a[1]) + int(a[2])
+    sum_2 = int(a[3]) + int(a[4]) + int(a[5])
+    if sum_1 == sum_2:
+        print("Lucky numbers ) !")
+    else:
+        print("I apologize, but your number is not lucky ( ")
+
+
+# lucky_ticket()
+
+
+
