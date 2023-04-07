52fc99d8fee86e6a77a12c9acf7bde9e6021d1cd
Abrorbek Patidinov <darkorb869@gmail.com>
2023-04-07 21:40:11 +0300

    Task 2: Find the sum of the digits of a three-digit number.

---
+++ main.py
@@ -0,0 +1,10 @@
+# Homeworks in Python
+
+# Задача 2: Найдите сумму цифр трехзначного числа.
+def summ_of_three_numbers(a, b, c):
+    print("Task №2")
+    print("Summ of three numbers: ", a + b + c)
+
+
+# summ_of_three_numbers(1, 2, 3)
+
