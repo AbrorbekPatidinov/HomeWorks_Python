801cecf7d1206637d2f5b5bb871b4f02377f2ff2
Abrorbek Patidinov <darkorb869@gmail.com>
2023-04-07 21:41:51 +0300

    Task 4: Petya, Katya and Seryozha make paper cranes. Together they made S cranes. How many cranes did each child make if it is known that Petya and Seryozha made the same number of cranes, and Katya made twice as many cranes as Petya and Seryozha together?

--- main.py
+++ main.py
@@ -8,3 +8,17 @@ def summ_of_three_numbers(a, b, c):
 
 # summ_of_three_numbers(1, 2, 3)
 
+
+# Задача 4: Петя, Катя и Сережа делают из бумаги журавликов. Вместе они сделали S журавликов.
+# Сколько журавликов сделал каждый ребенок, если известно, что Петя и Сережа сделали одинаковое количество журавликов,
+# а Катя сделала в два раза больше журавликов, чем Петя и Сережа вместе?
+def petya_katya_seryozha_made_paper_cranes(s, x, k):
+    print("Task №4")
+    s = int(input())
+    x = s // 6
+    k = (x + x) * 2
+    print("Cranes are: ", x, k, x)
+
+
+# petya_katya_seryozha_made_paper_cranes(2, 2, 4)
+
