7023fd38ea39809130744725ea6e00f5d1d5489b
Abrorbek Patidinov <darkorb869@gmail.com>
2023-04-19 14:41:11 +0300

    Problem 16: It is required to calculate how many times a certain number X occurs in the array A[1..N]. The user in the first line enters a natural number N - the number of elements in the array. The next lines contain N integers Ai. The last line contains the number X

--- Console/main.py
+++ Console/main.py
@@ -1,18 +1,20 @@
 # Homeworks in Python
+import random
+
 
 # Задача 2: Найдите сумму цифр трехзначного числа.
-def summ_of_three_numbers(a, b, c):
+def task_2(a, b, c):
     print("Task №2")
     print("Summ of three numbers: ", a + b + c)
 
 
-# summ_of_three_numbers(1, 2, 3)
+# task_2(1, 2, 3)
 
 
 # Задача 4: Петя, Катя и Сережа делают из бумаги журавликов. Вместе они сделали S журавликов.
 # Сколько журавликов сделал каждый ребенок, если известно, что Петя и Сережа сделали одинаковое количество журавликов,
 # а Катя сделала в два раза больше журавликов, чем Петя и Сережа вместе?
-def petya_katya_seryozha_made_paper_cranes(s, x, k):
+def task_4(s, x, k):
     print("Task №4")
     s = int(input())
     x = s // 6
@@ -20,13 +22,13 @@ def petya_katya_seryozha_made_paper_cranes(s, x, k):
     print("Cranes are: ", x, k, x)
 
 
-# petya_katya_seryozha_made_paper_cranes(2, 2, 4)
+# task_4(2, 2, 4)
 
 
 # Задача 6: Вы пользуетесь общественным транспортом? Вероятно, вы расплачивались за проезд и получали билет с номером.
 # Счастливым билетом называют такой билет с шестизначным номером, где сумма первых трех цифр равна сумме последних трех.
 # Т.е. билет с номером 385916 – счастливый, т.к. 3+8+5=9+1+6. Вам требуется написать программу, которая проверяет счастливость билета.
-def lucky_ticket():
+def task_6():
     print("Task №6")
     a = input("Enter your numbers on your ticket: ")
     sum_1 = int(a[0]) + int(a[1]) + int(a[2])
@@ -37,12 +39,12 @@ def lucky_ticket():
         print("I apologize, but your number is not lucky ( ")
 
 
-# lucky_ticket()
+# task_6()
 
 
 # Задача 8: Требуется определить, можно ли от шоколадки размером n × m долек отломить k долек,
 # если разрешается сделать один разлом по прямой между дольками (то есть разломить шоколадку на два прямоугольника).
-def chocolate_task():
+def task_8():
     print("Task №8")
     n = int(input("Enter chocolate columns: "))
     m = int(input("Enter chocolate rows: "))
@@ -53,7 +55,7 @@ def chocolate_task():
         print("My apologize, but you can't take or even eat the chocolate !")
 
 
-# chocolate_task()
+# task_8()
 
 
 #  Задача 10:
@@ -62,7 +64,8 @@ def chocolate_task():
 # перевернуть, чтобы все монетки были повернуты вверх одной и той же
 # стороной. Выведите минимальное количество монет, которые нужно
 # перевернуть
-def coin_task():
+def task_10():
+    print("Task №10")
     n = int(input())
     coin_zero = 0
     coin_one = 0
@@ -78,14 +81,15 @@ def coin_task():
         print("Coins with '0' is more than '1' !")
 
 
-# coin_task()
+# task_10()
 
 
 # Задача 12: Петя и Катя – брат и сестра. Петя – студент, а Катя – школьница.
 # Петя помогает Кате по математике. Он задумывает два натуральных числа X и Y (X,Y≤1000), а Катя должна их отгадать.
 # Для этого Петя делает две подсказки. Он называет сумму этих чисел S и их произведение P.
 # Помогите Кате отгадать задуманные Петей числа.
-def petya_and_katya_guess_number_task():
+def task_12():
+    print("Task №12")
     number_1 = int(input("Enter the first number: "))
     number_2 = int(input("Enter the second number: "))
     for i in range(number_1):
@@ -95,11 +99,12 @@ def petya_and_katya_guess_number_task():
 
 
 
-# petya_and_katya_guess_number_task()
+# task_12()
 
 
 # Задача 14: Требуется вывести все целые степени двойки (т.е. числа вида 2k), не превосходящие числа N.
-def degree_number_that_not_increases_than_N_task():
+def task_14():
+    print("Task №14")
     n = int(input("Enter number: "))
     index = 0
     while 2 ** 1 <= n:
@@ -107,5 +112,26 @@ def degree_number_that_not_increases_than_N_task():
         index += 1
 
 
-# degree_number_that_not_increases_than_N_task()
+# task_14()
+
+
+# Задача 16: Требуется вычислить, сколько раз встречается некоторое число X в массиве A[1..N].
+# Пользователь в первой строке вводит натуральное число N – количество элементов в массиве.
+# В последующих  строках записаны N целых чисел Ai. Последняя строка содержит число X
+def task_16():
+    print("Task №16")
+    x = int(input("Enter number (from 1 to 10): "))
+    n = []
+    for i in range(1, 10):
+        n.append(random.randint(1, 10))
+    if x in n:
+        result = n.count(x)
+        print("Numbers are: ", n)
+        print("Your number: ", x)
+        print("Count: ", result)
+    else:
+        print("There is no number such as you entered !")
+
+
+task_16()
 
