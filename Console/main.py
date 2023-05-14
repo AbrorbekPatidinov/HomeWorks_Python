c23d2adacaf949fc4d6c21b3fac52f67e99dd899
Abrorbek Patidinov <darkorb869@gmail.com>
2023-05-14 21:05:37 +0300

    Problem 32: Determine the indexes of array (list) elements whose values belong to a given range (i.e. not less than a given minimum and not more than a given maximum)

--- Console/main.py
+++ Console/main.py
@@ -284,3 +284,19 @@ def task_32():
 
 
 # task_32()
+
+
+# Задача 32: Определить индексы элементов массива (списка), значения которых принадлежат заданному диапазону
+# (т.е. не меньше заданного минимума и не больше заданного максимума)
+def task_34():
+    list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+    min_number = int(input("Enter the minimum number: "))
+    max_number = int(input("Enter the maximum number: "))
+    for i in range(len(list_1)):
+        if min_number <= list_1[i] <= max_number:
+            print(i)
+
+
+# task_34()
+
+
